a67d2cc1843930c615a5a612d7268ca8ccd7fc54
Fridagtt <fridagtzm@gmail.com>
2020-02-27 15:22:03 -0600

    Initial Commit

---
+++ retoMasterMind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  retoMasterMind
+//
+//  Created by Frida Gutiérrez Mireles on 27/02/20.
+//  Copyright © 2020 Frida Gutiérrez Mireles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
